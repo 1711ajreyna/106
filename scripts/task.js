939ba958792409db5fd77d5ff5965c451e057639
Andrew Reyna <1711ajreyna@gmail.com>
2024-03-01 14:10:50 -0800

    budget update

--- scripts/task.js
+++ scripts/task.js
@@ -1,10 +1,12 @@
 class task {
-    constructor(title,description,color,date,status,budget){
+    constructor(title,description,color,date,status,budget,minRange,maxRange){
         this.title = title;
         this.description = description;
         this.color = color;
         this.date = date;
         this.status = status;
         this.budget = budget;
+        this.minRange = minRange;
+        this.maxRange = minRange;
     }
 }
